499fad3b5ac85c828a4c2f290c22b5b372122421
sumit011-ux <sunarsumit011@gmail.com>
2025-11-18 09:14:59 +0545

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33abc";
 
-console.log(typeof score);
-let valueInNumber = Number(score);
-console.log("After conversion");
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof score);
+// let valueInNumber = Number(score);
+// console.log("After conversion");
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33"=>33
 // "33abc"=>NaN
@@ -14,5 +14,59 @@ console.log(valueInNumber);
 
 // null=>0
 // undefined=>NaN
+
+
+// ********Operations********
+let value = 3
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+// let str1 = "Sumit";
+// let str2 = " Sunar";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(2+2+"1");
+
+// console.log(+true);
+// console.log(+"");
+// console.log(+null);
+// console.log(+undefined);
+// console.log(+"34abc");
+
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+gameCounter--;
+console.log(gameCounter);
+--gameCounter;
+console.log(gameCounter);
+gameCounter += 5;
+console.log(gameCounter);
+gameCounter -= 5;
+console.log(gameCounter);
+gameCounter *= 2;
+console.log(gameCounter);
+gameCounter /= 2;
+console.log(gameCounter);
+gameCounter %= 3;
+console.log(gameCounter);
+gameCounter **= 3;
+console.log(gameCounter);
+// ++ -- += -= *= /= %= **=
+
+
 
 
