9f7432a91e2f573a72b363562864f307e1f9f7e7
sumit011-ux <sunarsumit011@gmail.com>
2025-11-18 17:40:33 +0545

    comparision operator

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,43 @@
+// video6
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2=='2');
+// console.log(2==='2');
+// console.log(2!='2');
+// console.log(2!=='2');
+
+
+// console.log(2>='2');
+// console.log(2<='2');    
+// console.log(2=='2');
+// console.log(2==='2'); 
+// console.log(2!='2');
+// console.log(2!=='2');
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.log(null == 0);
+// console.log(null != 0);
+// console.log(null === 0);
+// console.log(null !== 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+// console.log(undefined == 0);
+// console.log(undefined != 0);
+// console.log(undefined === 0);
+// console.log(undefined !== 0); 
+// console.log(undefined == null);
+// console.log(undefined === null);    
+// console.log(undefined != null);
+// console.log(undefined !== null);    
